ba1fe3931e3759952b33c1d1d75a4dc868bc83b1
duchess <jerinakash1999@gmail.com>
2024-08-09 12:02:55 +0800

    small changes

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -29,22 +29,18 @@ const Login = () => {
               <div className="pass-input-div">
                 <input type={showPassword ? "text" : "password"} placeholder="Password" />
                 {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
+                <input type={showPassword ? "text" : "password"} placeholder="Confirm Password" />
                 
               </div>
 
               <div className="login-center-options">
                 <div className="remember-div">
-                  <input type="checkbox" id="remember-checkbox" />
-                  <label htmlFor="remember-checkbox">
-                    Remember for 30 days
-                  </label>
+               
                 </div>
-                <a href="#" className="forgot-pass-link">
-                  Forgot password?
-                </a>
+                
               </div>
               <div className="login-center-buttons">
-                <button type="button">Log In</button>
+                <button type="button">Sign up</button>
                 <button type="button">
                   <img src={GoogleSvg} alt="" />
                   Log In with Google
