6fa40b3b93ca33494e18682af3715d1a820cb28d
Zhan Rong <tonadotirunt@gmail.com>
2024-08-10 15:46:11 +0800

    added fields to POST /extractors

--- llm/ner-llm/server/api/extractors.py
+++ llm/ner-llm/server/api/extractors.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
+from typing import Any, Dict
 
 
 router = APIRouter(
@@ -7,6 +9,30 @@ router = APIRouter(
     responses = {404 : {"description" : "Not found"}}
 )
 
+
+class CreateExtractor(BaseModel) : 
+    name : str = Field(default="", description="Name of the extractor e.g. BaseEntityExtractor, MachineEntityExtractor. OPTIONAL")
+    description : str = Field(
+        default ="", description = "Description of the extractor. OPTIONAL"
+    )
+    
+    # json schema must be a valid json schema, string : any
+    json_schema : Dict[str, Any] = Field (
+        ... , description = "JSON schema for the extractor. REQUIRED", alias ="schema"
+    )
+    instruction : str = Field(..., description = "instructor for the extractor. REQUIRED")
+
 @router.get("/")
 def get():
     return {"message" : "Get all extractors"}
+
+
+
+
+@router.post("")
+
+def create_extractor(
+    create_request : CreateExtractor
+):
+    return {"message" : "Create extractor", "data" : create_request.dict()}
+    
