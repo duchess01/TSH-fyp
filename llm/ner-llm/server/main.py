9ea76377c6a97fa3dcf012845fc187f1906d2c4d
Zhan Rong <tonadotirunt@gmail.com>
2024-08-10 15:28:47 +0800

    initialize llm-nera adapter
    - uses fastAPI on port 8000

---
+++ llm/ner-llm/server/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+import logging
+from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title = "NER using LLM",
+    description = "Named Entity Recognition using Language Model",
+    version = "0.1",
+    openapi_tags= [
+        {
+            "name" : "NER", 
+            "description" : "Operations related to Named Entity Recognition using Language Model"
+        }
+    ],
+    debug =True
+)
+
+
+ROOT = Path(__file__).parent.parent.parent
+
+
+app.add_middleware(
+    CORSMiddleware,
+    
+    # TODO :update this to the endpoints calling this service
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+    
+    
+)
+
+@app.get("/ready")
+def ready():
+    return {"status" : "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run(app, host="localhost", port=8000)
