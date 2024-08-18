ca8eff0c8832e96aefacd26ac2c7b4b73f8625b1
duchess <jerinakash1999@gmail.com>
2024-08-18 23:13:55 +0800

    space

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -167,7 +167,6 @@ const Register = () => {
                         <button class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
                     </form>
           </div>
-
           <p className="login-bottom-p"> 
             Already have an account? <a href="#">Sign in</a>
           </p>
