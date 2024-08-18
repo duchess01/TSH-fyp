1fc7e52d065d4b9964043d6696b4b558d77315ea
duchess <jerinakash1999@gmail.com>
2024-08-18 23:07:28 +0800

    button

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -164,7 +164,7 @@ const Register = () => {
                             Must match the first password input field.
                         </p>
 
-                        <button  disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+                        <button class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
                     </form>
           </div>
 
