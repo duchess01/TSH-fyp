a0093aed9be68085ae67dedaf7fbad9d18854757
Davin Shee <105922278+DavinShee@users.noreply.github.com>
2024-09-02 21:05:39 +0800

    Testing when test case fails

--- backend/src/routes/healthcheck/__tests__/healthcheck.test.js
+++ backend/src/routes/healthcheck/__tests__/healthcheck.test.js
@@ -9,7 +9,7 @@ app.use("/api", healthcheckRouter);
 test("GET /api/health should return status UP and a timestamp", async () => {
   const response = await request(app).get("/api/health");
 
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(201);
   expect(response.body.status).toBe("UP");
   expect(response.body.timestamp).toBeDefined();
   expect(new Date(response.body.timestamp)).toBeInstanceOf(Date);
