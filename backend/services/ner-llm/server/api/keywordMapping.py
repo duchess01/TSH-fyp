c8f6c90131672f4552e891cb527c2b9372078c0a
Zhan Rong <tonadotirunt@gmail.com>
2024-09-08 17:44:03 +0800

    added /keywords PUT,DEL route

--- backend/services/ner-llm/server/api/keywordMapping.py
+++ backend/services/ner-llm/server/api/keywordMapping.py
@@ -4,7 +4,7 @@ from server.models.keywords import KeywordMappingRequest
 from db.dbconfig import get_session
 from db.models import KeywordMapping
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.exc import SQLAlchemyError
 
 
@@ -110,4 +110,103 @@ def createNamespaceKeywordMapping(
             status_code = 500, detail = f"Internal server error : {str(e)}"
         )
         
+    
+@router.put("", summary = "update a keyword mapping into postgres", description="update a keyword mapping to ensure that the model can recognize the keywords as a namespace")
+def updateNamespaceKeywordMapping(
+    keyword_request : KeywordMappingRequest,
+    
+    # depends allows to use the same session in the same request
+    session : Session = Depends(get_session),
+) -> GenericResponse : 
+    
+    try:
+    
+        for namespaceIndex in range(len(keyword_request.keywordArray)) : 
+        # first block to check whether all namespaces exist in the db
+            # check if k exist
+            
+            itemObject = keyword_request.keywordArray[namespaceIndex]
+            
+            namespace = ""
+            
+            for k in itemObject:
+                namespace = k 
+        
+            keywordArray = itemObject[namespace]
+            
+            stmt = select(KeywordMapping).where(KeywordMapping.namespace == namespace)
+            
+            result = session.execute(stmt)
+            
+            res = result.scalars().first()
+            
+            
+            if res is None :
+                raise HTTPException(
+                    status_code = 404, detail = f"Namespace record, {namespace} does not exist, try POST instead of PUT"
+                )
+                    
+        # second block to update the record
+                
+        for namespaceIndex in range(len(keyword_request.keywordArray)) : 
+            # check if k exist
+            
+            itemObject = keyword_request.keywordArray[namespaceIndex]
+            
+            namespace = ""
+            
+            for k in itemObject:
+                namespace = k 
+        
+            keywordArray = itemObject[namespace]
+      
+            
+        # update the record
+            session.query(KeywordMapping).filter(KeywordMapping.namespace == namespace).update({KeywordMapping.keywordArray : keywordArray})
+
+        
+        
+            
+
+            
+        session.commit()
+            
+        return GenericResponse(message= "PUT keyword mapping success, updated into db", data = keyword_request.keywordArray)
+    
+    except SQLAlchemyError as e:
+              # handle other exceptions
+        raise HTTPException(
+            status_code = 500, detail = f"Internal server error : {str(e)}"
+        )
+        
+        
+    
+@router.delete("/{namespace}", summary = "delete a keyword mapping by namespace", description="delete a keyword mapping by namespace")
+def deleteMappingByNamespace(
+    namespace :str , 
+    session : Session = Depends(get_session)
+) -> GenericResponse : 
+    
+    try:
+        stmt = select(KeywordMapping).where(KeywordMapping.namespace == namespace)
+        
+        result = session.execute(stmt)
+        
+        res = result.scalars().first()
+        
+        if res is None :
+            raise HTTPException(
+                status_code = 404, detail = f"Namespace {namespace} not found"
+            )
+        
+        session.delete(res)
+        
+        session.commit()
+        
+        return GenericResponse(message = "DELETE mapping by namespace success", data = res)
+    
+    except SQLAlchemyError as e :
+        raise HTTPException(
+            status_code = 500, detail = f"Internal server error : {str(e)}"
+        )
     
