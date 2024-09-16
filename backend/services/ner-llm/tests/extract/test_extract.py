b9d6b10e3aadf7517b715b66a709600382feeb0a
Kum Zhan Rong <zrkum.2021@scis.smu.edu.sg>
2024-09-16 21:47:06 +0800

    removed tests that require openAPI key

--- backend/services/ner-llm/tests/extract/test_extract.py
+++ backend/services/ner-llm/tests/extract/test_extract.py
@@ -8,16 +8,18 @@ from tests.extractors.mock_data import add_mock_data
 
 
 # @pytest.mark.asyncio
-def test_extract_with_extractor_success(client, 
-                           set_up_db,
-                           session
-):
-    # Test successful extraction
+
+# REQUIRES GPT3.5
+# def test_extract_with_extractor_success(client, 
+#                            set_up_db,
+#                            session
+# ):
+#     # Test successful extraction
     
-    db = set_up_db
-    TestClient = client
+#     db = set_up_db
+#     TestClient = client
     
-    instances, userIds = add_mock_data(session) 
+#     instances, userIds = add_mock_data(session) 
     
     
     
@@ -25,17 +27,17 @@ def test_extract_with_extractor_success(client,
     
     
 
-    request_payload = {
-        "text" : "Regarding the panasonic MH320's axis wheel, what is the tool length compensation feature for machines having multiple rotary axes?",
-        "extractor_id" : userIds[0],
-        "model_name" : "gpt-3.5-turbo"
-    }
+#     request_payload = {
+#         "text" : "Regarding the panasonic MH320's axis wheel, what is the tool length compensation feature for machines having multiple rotary axes?",
+#         "extractor_id" : userIds[0],
+#         "model_name" : "gpt-3.5-turbo"
+#     }
 
-    response = TestClient.post('/extract', json=request_payload)
+#     response = TestClient.post('/extract', json=request_payload)
 
-    assert response.status_code == 200
-    assert response.json()['message'] == "success"
-    assert 'data' in response.json()
+#     assert response.status_code == 200
+#     assert response.json()['message'] == "success"
+#     assert 'data' in response.json()
 
 def test_extract_with_extractor_missing_text(client, 
                            set_up_db,
@@ -79,26 +81,28 @@ def test_extract_with_extractor_not_found(client,
     assert response.status_code == 404
     assert response.json()['message'] == "Extractor not found"
 
-def test_extract_output_correctness(
-    client, set_up_db, session
-) : 
-    db = set_up_db
-    TestClient = client
-    instances, userIds = add_mock_data(session)
+
+# NEED GPT3.5
+# def test_extract_output_correctness(
+#     client, set_up_db, session
+# ) : 
+#     db = set_up_db
+#     TestClient = client
+#     instances, userIds = add_mock_data(session)
     
-    requestPayload = {
-        "text" : "What is the maximum speed of the CNC Milling Machine when using the automatic tool changer feature to swap the spindle?",
-        "extractor_id" : userIds[0],
-        "model_name" : "gpt-3.5-turbo"
-    }
+#     requestPayload = {
+#         "text" : "What is the maximum speed of the CNC Milling Machine when using the automatic tool changer feature to swap the spindle?",
+#         "extractor_id" : userIds[0],
+#         "model_name" : "gpt-3.5-turbo"
+#     }
     
-    response = TestClient.post('/extract', json=requestPayload)
+#     response = TestClient.post('/extract', json=requestPayload)
     
-    print(response.json(), 'response')
+#     print(response.json(), 'response')
     
-    assert response.status_code == 200
-    assert response.json()['message'] == "success"
-    assert response.json()['data'][0]['properties']['feature'] == "automatic tool changer"
-    assert response.json()['data'][0]['properties']['name'] == "CNC Milling Machine"
-    assert response.json()['data'][0]['properties']['part'] == "spindle"
+#     assert response.status_code == 200
+#     assert response.json()['message'] == "success"
+#     assert response.json()['data'][0]['properties']['feature'] == "automatic tool changer"
+#     assert response.json()['data'][0]['properties']['name'] == "CNC Milling Machine"
+#     assert response.json()['data'][0]['properties']['part'] == "spindle"
     
