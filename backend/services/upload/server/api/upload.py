227611a7ac467f664403e203fee8f412615c5c62
poojunsui <My name>
2024-10-21 03:12:27 +0800

    integration for upload feature

--- backend/services/upload/server/api/upload.py
+++ backend/services/upload/server/api/upload.py
@@ -7,19 +7,14 @@ import tempfile
 import os
 import shutil
 import traceback
+import requests
 
 from utils.process_manuals import process_headings, save_output_to_file
-
-
 
 router = APIRouter(
     prefix = "/upload",
     tags = ["Upload PDF for processing"],
 )
-
-
-
-
 
 @router.post("", summary = "upload a pdf file", description="endpoint to upload a pdf file, file will be processed.")
 async def uploadPdf(
@@ -32,12 +27,6 @@ async def uploadPdf(
     text = ""
     
     try : 
-        # reader = PdfReader()
-        # for page in reader.pages:
-        #     text += page.extract_text() + "\n"
-        #     print("TEXT EXTRACTED: ", text)
-        
-        
         # PROCESS UPLOADED FILE AS A TMP FILE
         tmp_dir = tempfile.mkdtemp()
         tmp_file_path = os.path.join(tmp_dir, file.filename)
@@ -47,18 +36,48 @@ async def uploadPdf(
             
         relative_url = os.path.relpath(tmp_file_path)
         
+        # create a manual record in the database
+        pdf_file = relative_url.split("\\")[-1].split(".")[0].lower().replace("_", "-")
+        print("pdf_file:", pdf_file, "create a manual")
+        url = "http://localhost:8000/manual/status" 
+        data = {
+            "manual_name": pdf_file,
+            "status": "in_progress"
+        }
+        response = requests.post(url, json=data)
+        
+        if response.status_code == 200:
+            print(f"Successfully updated status to in progress for file {pdf_file}")
+        else:
+            print(f"Failed to update status in the database. Status Code: {response.status_code}")
+        
+        
+        
         # process text
         extracted_content = run_process(relative_url)
         
-        
-        # run NER to extract pages 
+        # run NER to extract keywords + embed keywords
         processed_output = process_headings(extracted_content)
         
-        file_name = file.filename.split("\\")[-1].split(".")[0]
+        file_name = file.filename.split("\\")[-1].split(".")[0].lower().replace("_", "-")
         
         file_name = f"{file_name}.json"
         # Upsert to file 
         save_output_to_file(processed_output, file_name)
+
+        # update database to status = success
+        print("pdf_file:", pdf_file, "create manual in db")
+        url = "http://localhost:8000/manual/create" 
+        data = {
+            "manual_name": pdf_file,
+            "manual_mappings": processed_output
+        }
+        response = requests.post(url, json=data)
+        
+        if response.status_code == 200:
+            print(f"Successfully created file {pdf_file} in database ")
+        else:
+            print(f"Failed to update status in the database. Status Code: {response.status_code}")
         
             
             
@@ -86,14 +105,3 @@ async def uploadPdf(
                 
             }
         )
-    
-    
-   
-    
-    
-    
-    
-
-        
-        
-    
